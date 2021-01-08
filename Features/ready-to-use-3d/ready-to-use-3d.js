908981c1ccb1773b270899a004780863e0304026
jli <jli@SCICELL.com>
2021-01-08 11:54:15 -0800

    tooltip

--- Features/ready-to-use-3d/ready-to-use-3d.js
+++ Features/ready-to-use-3d/ready-to-use-3d.js
@@ -1,11 +1,12 @@
 const mainContainer = document.querySelector(".main-container");
+const imgContainer = document.querySelector(".img-container");
 const bodyImg = document.querySelector("#body-img");
 const circles = document.querySelectorAll(".circle-path");
-const tooltips = document.querySelectorAll(".tooltip");
+// const tooltips = document.querySelectorAll(".tooltip");
 
 // cerebral-cortex
 const cerebralCortex = document.querySelector("#cerebral-cortex");
-const cerebralCortexTT = document.querySelector("#cerebral-cortex-tt");
+// const cerebralCortexTT = document.querySelector("#cerebral-cortex-tt");
 
 cerebralCortex.addEventListener("click", () => {
   window.open(
@@ -14,25 +15,33 @@ cerebralCortex.addEventListener("click", () => {
   );
 });
 
-cerebralCortex.addEventListener("mouseenter", function () {
+// add tooltip
+cerebralCortex.appendTooltip = function () {
+  console.log("oo");
+  const cerebralCortexTT = document.createElement("div");
+  cerebralCortexTT.id = "cerebralCortexTT";
+  cerebralCortexTT.className = "tooltip";
+  cerebralCortexTT.innerHTML = "<div>Cerebral Cortex Model</div>";
   let sel = this;
   let pos = sel.getBoundingClientRect();
   cerebralCortexTT.style.display = "block";
-  cerebralCortexTT.style.top = pos.top - 40 + "px";
-  cerebralCortexTT.style.left = pos.left - 70 + "px";
-  cerebralCortex.setAttribute("r", "6");
-});
+  cerebralCortexTT.style.top = pos.top - 30 + "px";
+  cerebralCortexTT.style.left = pos.left - 60 + "px";
+  imgContainer.appendChild(cerebralCortexTT);
+};
+cerebralCortex.appendTooltip();
 
-cerebralCortex.addEventListener("mouseleave", () => {
-  setTimeout(() => {
-    cerebralCortexTT.style.display = "none";
-    cerebralCortex.setAttribute("r", "4");
-  }, 3000);
-});
+// cerebralCortex.addEventListener("mouseenter", function () {
+//   let sel = this;
+//   let pos = sel.getBoundingClientRect();
+//   cerebralCortexTT.style.display = "block";
+//   cerebralCortexTT.style.top = pos.top - 18 + "px";
+//   cerebralCortexTT.style.left = pos.left - 70 + "px";
+// });
 
 // ocular
 const ocular = document.querySelector("#ocular");
-const ocularTT = document.querySelector("#ocular-tt");
+// const ocularTT = document.querySelector("#ocular-tt");
 
 ocular.addEventListener("click", () => {
   window.open(
@@ -41,17 +50,98 @@ ocular.addEventListener("click", () => {
   );
 });
 
-ocular.addEventListener("mouseenter", function () {
-  let sel = this;
-  let pos = sel.getBoundingClientRect();
-  ocularTT.style.display = "block";
-  ocularTT.style.top = pos.top - 40 + "px";
-  ocularTT.style.left = pos.left - 40 + "px";
+// ocular.addEventListener("mouseenter", function () {
+//   let sel = this;
+//   let pos = sel.getBoundingClientRect();
+//   ocularTT.style.display = "block";
+//   ocularTT.style.top = pos.top - 18 + "px";
+//   ocularTT.style.left = pos.left - 40 + "px";
+// });
+
+// blood-brain-barrier
+const bloodBrainBarrier = document.querySelector("#blood-brain-barrier");
+// const bloodBrainBarrierTT = document.querySelector("#blood-brain-barrier-tt");
+
+bloodBrainBarrier.addEventListener("click", () => {
+  window.open(
+    "https://www.sciencellonline.com/3d-ready-to-use-bbb-model",
+    "_self"
+  );
 });
 
-ocular.addEventListener("mouseleave", () => {
-  ocularTT.style.display = "none";
+// bloodBrainBarrier.addEventListener("mouseenter", function () {
+//   let sel = this;
+//   let pos = sel.getBoundingClientRect();
+//   bloodBrainBarrierTT.style.display = "block";
+//   bloodBrainBarrierTT.style.top = pos.top - 18 + "px";
+//   bloodBrainBarrierTT.style.left = pos.left - 80 + "px";
+// });
+
+// lung
+const lung = document.querySelector("#lung");
+// const lungTT = document.querySelector("#lung-tt");
+
+lung.addEventListener("click", () => {
+  window.open(
+    "https://www.sciencellonline.com/3d-ready-to-use-lung-models",
+    "_self"
+  );
 });
+
+// lung.addEventListener("mouseenter", function () {
+//   let sel = this;
+//   let pos = sel.getBoundingClientRect();
+//   lungTT.style.display = "block";
+//   lungTT.style.top = pos.top - 18 + "px";
+//   lungTT.style.left = pos.left - 40 + "px";
+// });
+
+// all circles
+const modelsArr = [
+  "Cerebral Cortex Model",
+  "Ocular Model",
+  "Blood Brain Barrier Model",
+  "Lung Model",
+  "Liver Model",
+  "Kidney Model",
+  "Mesenchymal Stem Cell Model",
+  "Cartilage Model",
+  "Bone Model",
+];
+// for (const circle of circles) {
+//   circle.appendTooltip = function () {
+//     const tooltip = document.createElement("div");
+//   };
+//   circle.appendTooltip();
+// }
+
+for (const circle of circles) {
+  circle.addEventListener("mouseenter", function () {
+    let sel = this;
+    // console.log(this);
+    let pos = sel.getBoundingClientRect();
+    // for (const tooltip of tooltips) {
+    //   tooltip.style.display = "block";
+    //   tooltip.style.top = pos.top - 18 + "px";
+    //   tooltip.style.left = pos.left - 40 + "px";
+    // }
+  });
+}
+
+for (const circle of circles) {
+  circle.addEventListener("mouseenter", function () {
+    circle.setAttribute("r", "6");
+  });
+
+  circle.addEventListener("mouseleave", function () {
+    // for (const tooltip of tooltips) {
+    //   setTimeout(() => {
+    //     tooltip.style.opacity = "0.2";
+    //   }, 500);
+    // }
+    circle.setAttribute("r", "4");
+  });
+}
 
 // [].forEach.call(document.querySelectorAll(".circle-path"), (item)=>{
 
