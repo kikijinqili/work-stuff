0e3b9b709d20c7d781153c58671a55487605158f
kikijinqili <jinqi.li.310@gmail.com>
2021-01-11 14:55:55 -0800

    red blink

--- Features/ready-to-use-3d/ready-to-use-3d.js
+++ Features/ready-to-use-3d/ready-to-use-3d.js
@@ -3,7 +3,7 @@ const imgContainer = document.querySelector(".img-container");
 const bodyImg = document.querySelector("#body-img");
 const circles = document.querySelectorAll(".circle-path");
 
-// open url
+// urls
 const urlsArr = [
   "https://www.sciencellonline.com/ready-to-use-3d-human-cortical-spheroids.html",
   "https://www.sciencellonline.com/products-services/cell-based-assays/3d-cell-culture/3d-ready-to-use-kits/ocular-model.html",
@@ -16,71 +16,25 @@ const urlsArr = [
   "https://www.sciencellonline.com/3d-ready-to-use-bone-models",
 ];
 
+// add links to circles
 for (const [i, circle] of circles.entries()) {
   circle.addEventListener("click", () => {
     window.open(urlsArr[i], "_self");
   });
 }
 
-// add tooltips
-const modelsArr = [
-  "Cerebral Cortex Model",
-  "Ocular Model",
-  "Blood Brain Barrier Model",
-  "Lung Models",
-  "Liver Models",
-  "Kidney Model",
-  "Mesenchymal Stem Cell Model",
-  "Cartilage Model",
-  "Bone Models",
-];
-const idsArr = [
-  "cerebral-cortex-tt",
-  "ocular-tt",
-  "blood-brain-barrier-tt",
-  "lung-tt",
-  "liver-tt",
-  "kidney-tt",
-  "mesenchymal-stem-cell-tt",
-  "cartilage-tt",
-  "bone-tt",
-];
+// add links to tooltips
+const rects = document.querySelectorAll(".tooltip-rect");
+const texts = document.querySelectorAll(".tooltip-text");
 
-for (const [i, circle] of circles.entries()) {
-  circle.appendTooltip = function () {
-    const tooltipDiv = document.createElement("div");
-    tooltipDiv.className = "tooltip";
-    tooltipDiv.id = idsArr[i];
-    tooltipDiv.innerHTML = modelsArr[i];
-    let sel = this;
-    let pos = sel.getBoundingClientRect();
-    tooltipDiv.style.display = "block";
-    tooltipDiv.style.top = pos.top + 40 + "px";
-    tooltipDiv.style.left = pos.left - 35 + "px";
-    imgContainer.appendChild(tooltipDiv);
-  };
-  circle.appendTooltip();
+for (const [i, rect] of rects.entries()) {
+  rect.addEventListener("click", () => {
+    window.open(urlsArr[i], "_self");
+  });
 }
 
-const tooltips = document.querySelectorAll(".tooltip");
-
-// mouse event
-for (const [i, circle] of circles.entries()) {
-  circle.addEventListener("mouseenter", function () {
-    circle.setAttribute("r", "8");
-    for (const [j, tooltip] of tooltips.entries()) {
-      if (i === j) {
-        tooltip.style.opacity = "1";
-      }
-    }
-  });
-
-  circle.addEventListener("mouseleave", function () {
-    for (const tooltip of tooltips) {
-      setTimeout(() => {
-        tooltip.style.opacity = "0.3";
-      }, 500);
-    }
-    circle.setAttribute("r", "6");
+for (const [i, text] of texts.entries()) {
+  text.addEventListener("click", () => {
+    window.open(urlsArr[i], "_self");
   });
 }
